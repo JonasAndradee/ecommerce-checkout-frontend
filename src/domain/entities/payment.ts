21966f7e7aeb3312dcf07329dbf6b23835f966f5
Jonas Andrade <jonaswmandrade@gmail.com>
2025-02-02 13:57:00 -0400

    feat: add entitie Payment

---
+++ src/domain/entities/payment.ts
@@ -0,0 +1,27 @@
+import { PaymentType } from './common'
+
+export interface CardPaymentRequest {
+  number: string
+  holderName: string
+  cvv: string
+  expirationDate: string
+  installments: number
+}
+
+export interface CardPaymentResponse {
+  firstDigits: string
+  lastDigits: string
+  holderName: string
+  expirationDate: string
+  installments: number
+}
+
+export interface PaymentMethodRequest {
+  type: PaymentType
+  card: CardPaymentRequest
+}
+
+export interface PaymentMethodResponse {
+  type: PaymentType
+  card: CardPaymentResponse
+}
